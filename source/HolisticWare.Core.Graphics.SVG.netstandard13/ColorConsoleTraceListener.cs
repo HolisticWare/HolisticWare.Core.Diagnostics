a055e523911f7456caa033ca7b6044f867168419
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-04-04 12:58:31 +0200

    source

---
+++ source/HolisticWare.Core.Graphics.SVG.netstandard13/ColorConsoleTraceListener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Core.Diagnostics
+{
+    /// <summary>
+    /// 
+    /// .NET Standard	2.0
+    /// https://apisof.net/catalog/System.Diagnostics.TextWriterTraceListener
+    /// </summary>
+    public class ColorConsoleTraceListener
+        :
+        Core.Diagnostics.ConsoleTraceListener
+    {
+        public ColorConsoleTraceListener()
+            :
+            base(System.Console.Out)
+        {
+            return;
+        }
+
+        public ColorConsoleTraceListener(bool useErrorStream)
+            :
+            base(useErrorStream ? System.Console.Error : System.Console.Out)
+        {
+            return;
+        }
+
+        IEnumerable<(TraceEventType event, ConsoleColor color)> EventColors = null;
+
+    }
+}
---
+++ source/HolisticWare.Core.Graphics.SVG.netstandard13/TextWriterTraceListener.cs
@@ -0,0 +1,34 @@
+﻿namespace Core.Diagnostics
+{
+    /// <summary>
+    /// 
+    /// .NET Standard	2.0
+    /// https://apisof.net/catalog/System.Diagnostics.TextWriterTraceListener
+    /// </summary>
+    public class ConsoleTraceListener
+        :
+        System.Diagnostics.TextWriterTraceListener
+    {
+        public ConsoleTraceListener()
+            :
+            base(System.Console.Out)
+        {
+            return;
+        }
+
+        public ConsoleTraceListener(System.IO.TextWriter tw)
+            :
+            base(tw)
+        {
+            return;
+        }
+
+        public ConsoleTraceListener(bool useErrorStream)
+            :
+            base(useErrorStream ? System.Console.Error : System.Console.Out)
+        {
+            return;
+        }
+
+    }
+}
